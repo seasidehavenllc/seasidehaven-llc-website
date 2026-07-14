8f060b14c8436aaf70209d8d987e2dadba293eb8
DripSmoke <dakotajenks58@gmail.com>
2026-07-13 20:09:32 -0600

    f

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("mousemove", (e) => {
+  document.querySelectorAll(".bubble-card").forEach((card) => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  });
+});
